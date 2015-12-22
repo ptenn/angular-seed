a9a9aa9d08307c9714641205d095e8e5505bc71c
P.J. Tenn <ptenn@arbfile.org>
2015-12-22 13:44:55 -0500

    experimental code.

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,5 @@
-var app = angular.module('app', ['ui.router', 'ui.bootstrap']);
+var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'restangular']);
+
+app.run(function(Restangular) {
+  Restangular.setBaseUrl('restapi/v1');
+});
--- app/js/main/controllers/HomeCtrl.js
+++ app/js/main/controllers/HomeCtrl.js
@@ -1,9 +1,34 @@
-app.controller('HomeCtrl', ['$scope', function($scope) {
+app.controller('HomeCtrl', ['$scope', 'Restangular', function($scope, Restangular) {
   $scope.controllerSettings = {};
+
+  $scope.people = [
+    { 'firstName': 'Jane',
+      'lastName': 'Smith'},
+    { 'firstName': 'Thomas',
+      'lastName': 'Adams'},
+    { 'firstName': 'Joseph',
+      'lastName': 'Rogers'}
+  ];
 
   init();
 
   function init() {
     $scope.controllerSettings.name = "Philip";
+    Restangular
+      .one('users', 11)
+      .get()
+      .then(
+        function(response) {
+          console.log("Response " + response);
+        },
+        function(error)
+        {
+          console.log("an error occurred" + error);
+        }
+      );
+
+    var idx = _.findIndex($scope.people, {'firstName': 'Thomas'});
+    console.log('Index is:' + idx);
+
   }
 }]);
